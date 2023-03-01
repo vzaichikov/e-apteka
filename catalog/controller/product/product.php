c5d9eb8c24abfdb4dcdefc8d46e4ef0938bca316
v.zaichikov <v.zaichikov@gmail.com>
2023-03-01 16:01:46 +0200

    Many changes in main product controller

--- catalog/controller/product/product.php
+++ catalog/controller/product/product.php
@@ -635,6 +635,12 @@
 								} else {
 								$price = false;
 							}
+
+							if ((($this->config->get('config_customer_price') && $this->customer->isLogged()) || !$this->config->get('config_customer_price')) && (float)$option_value['special']) {
+								$special = $this->currency->format($this->tax->calculate($option_value['special'], $product_info['tax_class_id'], $this->config->get('config_tax') ? 'P' : false), $this->session->data['currency']);
+								} else {
+								$special = false;
+							}
 							
 							$product_option_value_data[] = array(
 							'product_option_value_id' => $option_value['product_option_value_id'],
@@ -642,6 +648,7 @@
 							'name'                    => $option_value['name'],
 							'image'                   => $this->model_tool_image->resize($option_value['image'], 50, 50),
 							'price'                   => $price,
+							'special'                 => $special,
 							'price_prefix'            => $option_value['price_prefix']
 							);
 						}
