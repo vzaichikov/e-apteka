1fb9bf6883a4eb2c82fda4aadf2c0eaf930c75ec
v.zaichikov <v.zaichikov@gmail.com>
2023-03-17 12:59:22 +0200

    Moved to non-cached queries in google auth controller

--- catalog/controller/api/google.php
+++ catalog/controller/api/google.php
@@ -34,7 +34,7 @@ class ControllerAPIGoogle extends Controller {
 			'social_id' 	=> $JWTPayload['sub']
 		);
                 
-        $customer_query = $this->db->query("SELECT * FROM oc_customer WHERE social_id = '" . (string)$this->db->escape($customer['social_id']) . "'");                
+        $customer_query = $this->db->ncquery("SELECT * FROM oc_customer WHERE social_id = '" . (string)$this->db->escape($customer['social_id']) . "'");                
         $customer_info 	= $customer_query->row;
 
         if ($customer_info) {
@@ -55,7 +55,7 @@ class ControllerAPIGoogle extends Controller {
         			$result = ['status' => true, 'message' => 'Logged you by email as ' . $customer['email']];                            
         		} else {
         			$customer_id = $this->model_account_customer->addCustomer($customer);
-        			$this->db->query("UPDATE oc_customer SET social_id = '" . (string)$this->db->escape($customer['social_id']) . "' WHERE customer_id = '" . (int)$customer_id . "'");
+        			$this->db->ncquery("UPDATE oc_customer SET social_id = '" . (string)$this->db->escape($customer['social_id']) . "' WHERE customer_id = '" . (int)$customer_id . "'");
 
         			if($customer_id){                            
         				$customer_info = $this->model_account_customer->getCustomer($customer_id);
