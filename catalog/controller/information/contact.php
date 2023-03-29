6add05ea0679fdd2b6b1d2fd9733a6c6709e7305
v.zaichikov <v.zaichikov@gmail.com>
2023-03-29 14:18:37 +0300

    Moved fake-dls functions to contact

--- catalog/controller/information/contact.php
+++ catalog/controller/information/contact.php
@@ -1,6 +1,65 @@
 <?php
 	class ControllerInformationContact extends Controller {
 		private $error = array();
+
+		private function ecurl($url){
+			$ch = curl_init();
+			curl_setopt($ch, CURLOPT_URL, $url);
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+			curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+			$out = curl_exec($ch);
+			curl_close($ch);
+
+			return $out;
+		}
+
+		private function download_dls($url) {
+			$html = $this->ecurl($url);
+
+			$doc = new DOMDocument();
+			$doc->loadHTML($html);
+
+			$html = str_replace('\images\\', '/images/', $html);
+
+			$links = array();
+			foreach ($doc->getElementsByTagName('link') as $link) {
+				if ($link->getAttribute('rel') == 'stylesheet') {
+					$links[] = $link->getAttribute('href');
+				}
+			}
+			foreach ($doc->getElementsByTagName('script') as $script) {
+				$links[] = $script->getAttribute('src');
+			}
+
+			foreach ($doc->getElementsByTagName('img') as $image) {
+				$links[] = str_replace('\\', '/', $image->getAttribute('src'));
+			}
+
+
+			$base_url = parse_url($url, PHP_URL_SCHEME) . '://' . parse_url($url, PHP_URL_HOST);
+			foreach ($links as $link) {
+				$local_path = DIR_APPLICATION . '../dls-data/' . $link;
+				$dir = DIR_APPLICATION . '../dls-data/' . pathinfo($link,  PATHINFO_DIRNAME);
+
+				if (!is_dir($dir)){
+					mkdir($dir, 0755, true);
+				}
+
+				file_put_contents($local_path, $this->ecurl($base_url . $link));
+
+				$html = str_replace($link, 'https://e-apteka.com.ua/dls-data' . $link, $html);
+			}
+
+			$html = str_replace('href="/"', ' href="https://pharmacy.dls.gov.ua"', $html);
+
+			return $html;
+
+		}
+
+		public function dls(){
+			$this->response->setOutput($this->download_dls('https://pharmacy.dls.gov.ua/check?EDRPOU=22974151'));
+		}
 		
 		public function drugstores(){
 			$this->load->language('information/contact');
